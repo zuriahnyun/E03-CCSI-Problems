66109831508cac8169504025e74c4de70f0a49d2
Zuriahn Yun <114603430+zuriahnyun@users.noreply.github.com>
2023-01-18 12:46:01 -0800

    Add files via upload

---
+++ E03 Problem 1 and 3.py
@@ -0,0 +1,5 @@
+number = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+final = number + number2 + number3
+print(number, "+", number2, "+", number3, "=",final,sep="")
